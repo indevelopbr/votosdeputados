200683276c0a5672507d63cfc1cd9f67f8400774
Raphael Silva <silva.raphael@outlook.com.br>
2025-02-21 15:43:15 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -12,6 +12,15 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+$host = $_SERVER['HTTP_HOST'] ?? 'default';
+
+// Define o arquivo .env baseado no domínio
+if ($host === 'anistia08dejaneiro.com.br' || $host === 'www.anistia08dejaneiro.com.br') {
+    putenv('LARAVEL_ENV=.env.anistia');
+} else {
+    putenv('LARAVEL_ENV=.env');
+}
+
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
